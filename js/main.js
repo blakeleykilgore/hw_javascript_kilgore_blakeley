cc3b411c6ba880a7f7a3f653149dcf3bc5097020
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-23 13:25:43 -0500

    Add function expression

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ function howdy( firstName, lastName, greeting ) {
 
 var message = howdy( 'Blakeley ', 'Kilgore ', 'says hi!' );
 console.log( message );
+
+var numba = function(string) {
+	if (string.length < 7) {
+		console.log('What a short little word!');
+	}
+	else if (string.length == 7) {
+		console.log('7, what a perfect choice!');
+	}
+	else {
+		console.log("I'm sorry, but that's too many to count");
+	}
+};
+
+numba("7");
+numba("seventy");
+numba("university");
