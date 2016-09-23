63ca1df6f7ca13e84ab038528e1fd63007ed9d44
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-23 13:09:31 -0500

    Add function declaration

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function howdy( firstName, lastName, greeting ) {
+    return firstName + lastName + greeting;
+}
+
+var message = howdy( 'Blakeley ', 'Kilgore ', 'says hi!' );
+console.log( message );
