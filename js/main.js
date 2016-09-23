71c6cf3a7871a850d77fd9e5697c89d937eaf8b0
Blakeley Kilgore <blakeleykilgore@gmail.com>
2016-09-23 12:49:04 -0500

    Add objects to var presidents and log Bill Clinton

---
+++ js/main.js
@@ -0,0 +1,40 @@
+var presidents = [
+
+{
+	firstName:"Barack",
+	lastName:"Obama",
+	termLength:"4 years",
+	party:"Democratic",
+	yearsOfPresidency:"8"
+},
+{
+	firstName:"George W.",
+	lastName:"Bush",
+	termLength:"4 years",
+	party:"Republican",
+	yearsOfPresidency:"8"
+},
+{
+	firstName:"Bill",
+	lastName:"Clinton",
+	termLength:"4 years",
+	party:"Democratic",
+	yearsOfPresidency:"8"
+},
+{
+	firstName:"George H.W.",
+	lastName:"Bush",
+	termLength:"4 years",
+	party:"Republican",
+	yearsOfPresidency:"4"
+},
+{
+	firstName:"Ronald",
+	lastName:"Reagan",
+	termLength:"4 years",
+	party:"Republican",
+	yearsOfPresidency:"8"
+}
+];
+
+console.log(presidents[2].firstName,  presidents[2].lastName);
